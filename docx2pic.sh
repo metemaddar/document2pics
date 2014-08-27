2f63f5ced7cb647f7cf68f2dd226484191d90fe1
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2014-08-28 01:03:33 +0430

    Update docx2pic.sh
    Now can do on wildcard.

--- docx2pic.sh
+++ docx2pic.sh
@@ -6,10 +6,13 @@ mkdir -p $mytmp
 mkdir -p $mytmp/extraction
 #Directory to fetch images from it
 mymedia=extraction/word/media/
+
+for etmfile in "$@"
+do
 #file name from input (maybe its path of file)
-fname=$(basename "$1")
+fname=$(basename "$etmfile")
 #file name with it's path plus -pictures
-dstpics="${1%.*}"-pictures
+dstpics="${etmfile%.*}"-pictures
 #extract the file extension
 extension="${fname##*.}"
 #exact file name
@@ -18,7 +21,7 @@ filename="${fname%.*}"
 #Now I just get docx's pictures. Because it rename them by image1 image2 ...
 if [[ "$extension" == "docx" ]]
 then
-	cp $1 ${mytmp}/${fname}.zip
+	cp $etmfile ${mytmp}/${fname}.zip
 	unzip -q ${mytmp}/${fname}.zip -d ${mytmp}/extraction
 	#directory to put images in it
 	mkdir -p $dstpics
@@ -27,3 +30,4 @@ then
 	#remove temp files.
 	rm -r $mytmp/*
 fi
+done
