36c92858f101b85c73d2d08a1319c3547173a492
mshcheglov <mshcheglov92@gmail.com>
2017-10-09 14:32:03 +0300

    small fix

--- docx2pic.sh
+++ docx2pic.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-result_dir=docx2pic_result
-tmp_dir=docx2pic_tmp
+result_dir="docx2pic_result"
+tmp_dir="docx2pic_tmp"
 
 while [ -d "${tmp_dir}" ]
 do
  tmp_dir="${tmp_dir}_"
 done
-mkdir -p $tmp_dir
+mkdir -p "${tmp_dir}"
 
 for filename in *.docx
 do
 	echo "Working with ${filename}"
 
+	unzip -q "${filename}" -d "${tmp_dir}"
+
 	dst_pics="${result_dir}/${filename%.*}"-pictures
-
-	mkdir -p $tmp_dir
-	cp "${filename}" "${tmp_dir}/${filename}".zip
-	unzip -q "${tmp_dir}/${filename}".zip -d "${tmp_dir}"
-
 	mkdir -p "${dst_pics}"
-	find "${tmp_dir}" -name '*.jp*' -exec cp {} "${dst_pics}" \;
-	rm -r "${tmp_dir}"
+	find "${tmp_dir}" -name 'image*' -exec cp {} "${dst_pics}" \;
+
+	rm -r "${tmp_dir}"/*
 done
 
-echo "All done"
+rm -r "${tmp_dir}"
 
+echo 'All done'
