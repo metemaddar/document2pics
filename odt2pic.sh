4be2992a17b81277d43a9febc60ec18354e3a10d
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2022-04-23 16:17:03 +0430

    Add odt2pic

---
+++ odt2pic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+result_dir='docx2pic_result'
+tmp_dir='docx2pic_tmp'
+
+while [ -d "${tmp_dir}" ]
+do
+ tmp_dir="${tmp_dir}_"
+done
+mkdir -p "${tmp_dir}"
+
+for filename in *.odt
+do
+	echo "Working with ${filename}"
+
+	unzip -q "${filename}" -d "${tmp_dir}"
+
+	dst_pics="${result_dir}/${filename%.*}"-pictures
+	mkdir -p "${dst_pics}"
+	cp "${tmp_dir}"/Pictures/* "${dst_pics}"/
+	rm -r "${tmp_dir}"
+done
+
+echo 'All done'
